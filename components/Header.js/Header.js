d7b453015f425906c79886da351ba5c4772cb487
Durgesh Kumar <code.durgesh@gmail.com>
2023-03-20 15:35:54 +0530

    init

---
+++ components/Header.js/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full flex items-center justify-center  py-3 border-b-2 px-2">
+      <div className="w-full flex items-center justify-center max-w-7xl">
+        <p className="text-3xl font-semibold text-center">
+          {" "}
+          KIIT E-CELL INTERNSHIP CAMP&apos;23
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '/styles/globals.css'
+import Header from "components/Header.js/Header";
+import "/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
